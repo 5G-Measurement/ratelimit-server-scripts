3042c64db4a89cb1b9ee85c282557bf35899a9a4
ahmadhassan997 <ahmadhassansandhu@gmail.com>
2021-01-11 23:08:04 -0600

    fix

--- ratelimit-server-setup.sh
+++ ratelimit-server-setup.sh
@@ -27,6 +27,3 @@ cd ~
 
 # set congestion control algorithm to cubic (for sanity)
 sudo sysctl -w net.ipv4.tcp_congestion_control=cubic
-
-# get faketcp binary
-git clone https://github.com/ahmadhassan997/rawtcp-udp.git
