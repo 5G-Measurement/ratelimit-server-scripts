2145f083c2262115717d12b8408c97b2d5035918
ahmadhassan997 <ahmadhassansandhu@gmail.com>
2021-01-12 18:47:54 -0600

    wip: ndt server script

--- setup-ndt-server.sh
+++ setup-ndt-server.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 ## Install docker [ubuntu]
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+# sudo apt-get update
+# sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo apt-key fingerprint 0EBFCD88
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# sudo apt-get update
+# sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 ## Install docker [centOS]
-# sudo yum install -y yum-utils
-# sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-# sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io
 
 ## Setup ndt-7 server
 https://github.com/m-lab/ndt-server.git
