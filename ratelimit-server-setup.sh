c01776b61e76647ab0ea71631f0ce30b0571bd14
ahmadhassan997 <ahmadhassansandhu@gmail.com>
2021-01-13 00:16:00 -0600

    change socket buffer values

--- ratelimit-server-setup.sh
+++ ratelimit-server-setup.sh
@@ -27,3 +27,15 @@ cd ~
 
 # set congestion control algorithm to cubic (for sanity)
 sudo sysctl -w net.ipv4.tcp_congestion_control=cubic
+
+# Change socket buffer sizes
+sudo sysctl -w net.core.rmem_default=31457280
+sudo sysctl -w net.core.rmem_max=536870912
+sudo sysctl -w net.core.wmem_default=31457280
+sudo sysctl -w net.core.wmem_max=536870912
+sudo sysctl -w net.ipv4.tcp_rmem='8192 16777216 536870912'
+sudo sysctl -w net.ipv4.tcp_wmem='8192 16777216 536870912'
+sudo sysctl -w net.ipv4.tcp_mem='786432 1048576 26777216'
+sudo sysctl -w net.ipv4.udp_mem='65536 131072 262144'
+sudo sysctl -w net.ipv4.udp_rmem_min=16384
+sudo sysctl -w net.ipv4.udp_wmem_min=16384
