73e4a3682fa478addb35b20962fa69379d29e0cd
ahmadhassan997 <ahmadhassansandhu@gmail.com>
2021-01-12 18:02:31 -0600

    wip: adding ndt server setup

---
+++ run-ndt-server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Run ndt-server container
+sudo docker run --network=bridge                \
+                --publish 443:4443              \
+                --publish 80:8080               \
+                --volume `pwd`/certs:/certs:ro  \
+                --volume `pwd`/datadir:/datadir \
+                --read-only                     \
+                --user `id -u`:`id -g`          \
+                --cap-drop=all                  \
+                ndt-server                      \
+                -cert /certs/cert.pem           \
+                -key /certs/key.pem             \
+                -datadir /datadir               \
+                -ndt7_addr :4443                \
+                -ndt7_addr_cleartext :8080
